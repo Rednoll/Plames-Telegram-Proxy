1eac218a09408bc5b54138a6cb181724799544db
Redn_oll <rednoll12@gmail.com>
2019-11-10 22:10:01 +0300

    dev

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
+const PORT = process.env.PORT || 8443;
+
 var proxy = require("http-proxy");
 
-proxy.createProxyServer({target:"https://inwaiders.enterprises/"}).listen(443);
+proxy.createProxyServer({target:"https://inwaiders.enterprises/"}).listen(80);
 
-proxy.createProxyServer({target:"https://api.telegram.org/"}).listen(8443);
+proxy.createProxyServer({target:"https://api.telegram.org/"}).listen(PORT);
