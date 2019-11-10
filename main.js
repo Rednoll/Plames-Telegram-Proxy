d1a6d4c3ade068d4c5ea1dd59e4aed00f199d5ec
Redn_oll <rednoll12@gmail.com>
2019-11-10 21:10:04 +0300

    dev

--- main.js
+++ main.js
@@ -1,1 +1,5 @@
-console.log('Hello World');
+var proxy = require("http-proxy");
+
+proxy.createProxyServer({target:"https://inwaiders.enterprises/"}).listen(443);
+
+proxy.createProxyServer({target:"https://api.telegram.org/"}).listen(8443);
