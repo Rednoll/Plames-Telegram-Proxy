9f4b3f91d899fe84bde07df4ffbc8b236d919ddf
Redn_oll <rednoll12@gmail.com>
2019-11-10 22:23:58 +0300

    dev

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 const PORT = process.env.PORT || 88;
-var app = express();
 
-app.listen(PORT, () => console.log(`Server is listening on port ${PORT}...`));
+console.log("Server is listening on port "+PORT+"...");
 
 var proxy = require("http-proxy");
 
